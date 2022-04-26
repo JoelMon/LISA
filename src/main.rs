25eb413e876ae9d68379b3e9d0da4001deaf0824
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-04-26 19:50:04 -0400

    Added ability to read in csv files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use serde::Deserialize;
+use std::fs::File;
+use std::io;
+use std::process;
+
+#[derive(Debug, Deserialize)]
+struct Po {
+    po: String,
+    style_code: String,
+    color_code: String,
+    msrp_size: String,
+    style_desc: String,
+    color_desc: String,
+    upc: String,
+    store_num: String,
+    qty: i64,
+}
+
+fn read_file() -> Result<()> {
+    let file_path = "examples/RFID.csv";
+    let file = File::open(file_path).context("Failed to open file")?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let results = read_file()?;
+    println!("{:?}", results);
+    Ok(())
 }
