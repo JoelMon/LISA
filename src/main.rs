9a77e57d856687c60494f3e172ca2a4d38c9d6c9
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-04-28 21:53:53 -0400

    WIP clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use anyhow::Ok;
 use anyhow::{Context, Result};
+use clap::Parser;
 use csv::StringRecord;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    #[clap(short, long)]
+    input: PathBuf,
+    #[clap(short, long)]
+    output: PathBuf,
+    #[clap(short, long)]
+    list: PathBuf,
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 #[allow(unused)]
@@ -20,8 +33,7 @@ struct Po {
     qty: String,
 }
 
-fn read_file() -> Result<Vec<StringRecord>> {
-    let file_path = "examples/RFID.csv"; // Hard coded path for debugging.
+fn read_file(file_path: PathBuf) -> Result<Vec<StringRecord>> {
     let file = File::open(file_path).context("Failed to open file")?;
     let mut rdr = csv::Reader::from_reader(file);
     let mut records: Vec<StringRecord> = vec![];
@@ -35,12 +47,11 @@ fn read_file() -> Result<Vec<StringRecord>> {
 
 fn filter_store(records: Vec<StringRecord>, list: Vec<&str>) -> Result<Vec<StringRecord>> {
     let mut filtered_records = vec![];
-    let store = "130"; // Store number hard coded for POC
 
     for num in list {
-        for each in records.clone().into_iter() {
-            if each.get(0).unwrap().to_string().contains(num) {
-                filtered_records.push(each)
+        for item in records.clone().into_iter() {
+            if item.get(0).unwrap().to_string().contains(num) {
+                filtered_records.push(item)
             }
         }
     }
@@ -56,60 +67,57 @@ fn has_rfid(record: &StringRecord) -> bool {
     return false;
 }
 
-// A hashset of unique store POs found in order to be able to create unique files
-fn found_stores(records: &StringRecord) -> HashSet<String> {
-    todo!();
-}
+fn write_file(records: Vec<StringRecord>, destination_path: PathBuf) -> Result<()> {
+    let store_list = records
+        .iter()
+        .map(|num| num.get(0).unwrap().to_owned())
+        .collect::<HashSet<String>>();
 
-fn write_file(records: Vec<StringRecord>) -> Result<()> {
-    let file_path = "examples/RFID_2.csv"; // Hard coded path for debugging.
-    let mut wtr = csv::Writer::from_writer(File::create(file_path)?);
+    for store in store_list {
+        let mut dest = destination_path;
+        let mut file_path = dest.set_file_name("test");
+        let mut wtr = csv::Writer::from_writer(File::create(file_path)?);
 
-    for each in records.iter() {
-        // Filter orders that has a '$' to qty "0".
-        if has_rfid(each) {
-            wtr.serialize(Po {
-                po: each.get(0).unwrap().to_owned(),
-                style_code: each.get(1).unwrap().to_owned(),
-                color_code: each.get(2).unwrap().to_owned(),
-                msrp_size: each.get(3).unwrap().to_owned(),
-                style_desc: each.get(4).unwrap().to_owned(),
-                color_desc: each.get(5).unwrap().to_owned(),
-                upc: each.get(6).unwrap().to_owned(),
-                store_num: "".to_owned(), // This field must an empty string
-                qty: "0".to_owned(),      // If it has RFID then set qty to 0
-            })?;
-        } else {
-            wtr.serialize(Po {
-                po: each.get(0).unwrap().to_owned(),
-                style_code: each.get(1).unwrap().to_owned(),
-                color_code: each.get(2).unwrap().to_owned(),
-                msrp_size: each.get(3).unwrap().to_owned(),
-                style_desc: each.get(4).unwrap().to_owned(),
-                color_desc: each.get(5).unwrap().to_owned(),
-                upc: each.get(6).unwrap().to_owned(),
-                store_num: "".to_owned(), // This field must an empty string
-                qty: each.get(8).unwrap().to_owned(),
-            })?;
+        for each in records.iter() {
+            // Filter orders that has a '$' to qty "0".
+            if has_rfid(each) && each.get(0).unwrap().to_owned() == store {
+                wtr.serialize(Po {
+                    po: each.get(0).unwrap().to_owned(),
+                    style_code: each.get(1).unwrap().to_owned(),
+                    color_code: each.get(2).unwrap().to_owned(),
+                    msrp_size: each.get(3).unwrap().to_owned(),
+                    style_desc: each.get(4).unwrap().to_owned(),
+                    color_desc: each.get(5).unwrap().to_owned(),
+                    upc: each.get(6).unwrap().to_owned(),
+                    store_num: "".to_owned(), // This field must an empty string
+                    qty: "0".to_owned(),      // If it has RFID then set qty to 0
+                })?;
+            } else if each.get(0).unwrap().to_owned() == store {
+                wtr.serialize(Po {
+                    po: each.get(0).unwrap().to_owned(),
+                    style_code: each.get(1).unwrap().to_owned(),
+                    color_code: each.get(2).unwrap().to_owned(),
+                    msrp_size: each.get(3).unwrap().to_owned(),
+                    style_desc: each.get(4).unwrap().to_owned(),
+                    color_desc: each.get(5).unwrap().to_owned(),
+                    upc: each.get(6).unwrap().to_owned(),
+                    store_num: "".to_owned(), // This field must an empty string
+                    qty: each.get(8).unwrap().to_owned(),
+                })?;
+            }
         }
+        wtr.flush()?;
     }
-    wtr.flush()?;
 
     Ok(())
 }
 
-fn split_files(records: Vec<StringRecord>) -> Result<Vec<StringRecord>> {
-    let fist_store = dbg!(records[0].get(0).unwrap().to_string());
-    let mut new_list: Vec<StringRecord> = Vec::new();
-
-    todo!()
-}
-
 fn main() -> Result<()> {
-    let store_list: Vec<&str> = vec!["127", "130"];
-    let results = read_file()?;
+    let arg = Args::parse();
+
+    let store_list: Vec<&str> = vec!["014", "071", "123", "010", "100"];
+    let results = read_file(arg.input)?;
     let results = filter_store(results, store_list)?;
-    let results = split_files(results)?;
-    write_file(results);
+    write_file(results, arg.output)?;
     Ok(())
 }
